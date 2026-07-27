de1f04a61bf08611c1d6bbf8fa96ef37f97f5598
Ubuntu <ubuntu@ip-172-31-33-164.eu-north-1.compute.internal>
2026-07-27 09:50:10 +0000

    fix: include sliderStatus in public sliders response (frontend filter needed it)

--- src/controllers/publicSliderController.js
+++ src/controllers/publicSliderController.js
@@ -3,7 +3,7 @@ import Slider from "../models/sliderModel.js";
 export const getPublicSliders = async (req, res) => {
   try {
     const sliders = await Slider.find({ sliderStatus: "Yes" })
-      .select("title sliderLink sliderOrder clientLogo")
+      .select("title sliderLink sliderOrder clientLogo sliderStatus")
       .sort({ sliderOrder: 1 });
 
     res.status(200).json({
